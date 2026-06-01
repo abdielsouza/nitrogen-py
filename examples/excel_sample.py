6ac1a8f5e52ecc8beb42b8ba5526ab2c52ddcc6b
abdielsouza <abdielcsouza@gmail.com>
2026-06-01 19:05:10 -0300

    chore: improved excel sample import organization.

--- examples/excel_sample.py
+++ examples/excel_sample.py
@@ -1,6 +1,7 @@
 import openpyxl
 import nitrogen.core as nt
 from nitrogen.backends.excel.backend import ExcelBackend
+from nitrogen.core.workbook import Workbook as NitrogenWorkbook
 
 class Products(nt.Sheet):
 	quantity = nt.Column(int)
@@ -14,7 +15,7 @@ Products.insert(quantity=10, price=1.2)
 # write to an Excel workbook using the provided backend
 wb = openpyxl.Workbook()
 backend = ExcelBackend(wb)
-from nitrogen.core.workbook import Workbook as NitrogenWorkbook
+
 nwb = NitrogenWorkbook()
 nwb.add(Products)
 nwb.sync(backend, path="products.xlsx")
