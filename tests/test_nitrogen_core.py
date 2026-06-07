9ee15da57f67ecff53abed39b40b7e436391198f
abdielsouza <abdielcsouza@gmail.com>
2026-06-07 15:59:09 -0300

    chore: update test files to the new relationship feature

--- tests/test_nitrogen_core.py
+++ tests/test_nitrogen_core.py
@@ -3,9 +3,15 @@ from pytest import Subtests
 
 def test_core(subtests: Subtests):
     class Products(nt.Sheet):
+        id = nt.Column(str)
         quantity = nt.Column(int)
         price = nt.Column(float)
         total = nt.Formula(quantity * price)
+
+    class Users(nt.Sheet):
+        name = nt.Column(str)
+        product_id = nt.Column(str)
+        product = nt.Relationship(Products, "product_id")
     
     with subtests.test("get formula variables"):
         print(f"\n{Products.total.dependencies}")
@@ -14,3 +20,7 @@ def test_core(subtests: Subtests):
         print(f"\n{Products.graph().nodes}")
         print(f"\n{Products.graph().execution_order()}")
         print(f"\n{Products.graph().affected_by("quantity")}")
+
+    with subtests.test("test for relationships"):
+        Products.insert(id="soap", quantity=4, price=3.50)
+        Users.insert(name="john", product_id="soap")
