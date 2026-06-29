bc7c1cd27fc7d729b9724900943330ad7783cbc6
abdielsouza <abdielcsouza@gmail.com>
2026-06-28 21:54:26 -0300

    feat(engine): update excel data source

--- src/nitrogen/engine/sources/excel.py
+++ src/nitrogen/engine/sources/excel.py
@@ -3,14 +3,20 @@ from nitrogen.engine.contexts import ExcelContext
 from nitrogen.engine.compilers.excel import ExcelCompiler
 from typing import cast, Any
 import openpyxl as xl
+import os
 
 class ExcelDataSource(DataSource):
     """Implementation of Excel data source."""
 
     def __init__(self, filepath: str):
-        self._workbook = xl.load_workbook(filepath)
+        if os.path.exists(filepath):
+            self._workbook = xl.load_workbook(filepath)
+        else:
+            self._workbook = xl.Workbook()
+
         self._context = ExcelContext()
         self._compiler = ExcelCompiler()
+        self._filepath = filepath
 
     def execute(self, query):
         self._context.worksheet = self._workbook[query.sheet]
@@ -18,3 +24,15 @@ class ExcelDataSource(DataSource):
         result = cast(Any, result)
         
         return result
+    
+    @property
+    def context(self):
+        return self._context
+
+    @property
+    def workbook(self):
+        return self._workbook
+    
+    @property
+    def filepath(self):
+        return self._filepath
